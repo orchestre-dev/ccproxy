787648c0bc658134a16cf709d1464a13ceb1f7fb
Praney Behl <praneybehl@gmail.com>
2025-07-22 15:12:21 +1000

    fix: Correct spelling of 'canceled' in comments

--- internal/pipeline/streaming.go
+++ internal/pipeline/streaming.go
@@ -53,8 +53,8 @@ func (p *StreamingProcessor) ProcessStreamingResponse(
 	go func() {
 		select {
 		case <-ctx.Done():
-			_ = reader.Close() // Safe to ignore: context cancelled, force close
-			_ = writer.Close() // Safe to ignore: context cancelled, force close
+			_ = reader.Close() // Safe to ignore: context canceled, force close
+			_ = writer.Close() // Safe to ignore: context canceled, force close
 		case <-done:
 			// Normal completion
 		}
